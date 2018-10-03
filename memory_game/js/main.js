6f735fb7416bdc25a5123b8e570139f0bc57ae33
Lovdeep Gahunia <lovdeep@Lovdeeps-MacBook-Pro.local>
2018-10-03 12:50:04 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,24 @@
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+var cardsInPlay = [cardOne];
+cardsInPlay.push (cardOne);
+
+var cardTwo = cards[2];
+var cardsInPlay = [cardTwo];
+cardsInPlay.push (cardTwo);
 
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+if (cardsInPlay.length === 2); {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+	alert("you found a match");
+} else {
+	alert("sorry, try again!");
+}
+}
+
+
 
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+console.log("User flipped " + cardTwo);
